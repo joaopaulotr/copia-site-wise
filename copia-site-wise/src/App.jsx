211c0d74e35f4ac9cdc868f0c35e4d42bc53d91c
Gustavo Pasquini <gustavopasquini2006@hotmail.com>
2025-02-12 22:51:42 -0300

    Conexão com a API

--- copia-site-wise/src/App.jsx
+++ copia-site-wise/src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import api from "./services/api";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [moedaDe, setMoedaDe] = useState("USD");
+  const [moedaPara, setMoedaPara] = useState("BRL");
+  const [cambio, setCambio] = useState(null);
+
+  const consultaCambio = (from, to) => {
+    api
+      .get(`/json/last/${from}-${to}`)
+      .then((response) => {
+        setCambio(response.data);
+        console.log("Dados do câmbio:", response.data);
+      })
+      .catch((err) => {
+        console.error("Ops! Ocorreu um erro: " + err);
+      });
+  };
+
+  // Chama a API quando moedaDe ou moedaPara mudar
+  useEffect(() => {
+    consultaCambio(moedaDe, moedaPara);
+  }, [moedaDe, moedaPara]);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <section className="navbar">
+        <h1 className="wise-title">WISE</h1>
+        {cambio && <p>Cotação Atual: {parseFloat(cambio?.[`${moedaDe}${moedaPara}`]?.bid)}</p>}
+      </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
